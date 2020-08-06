3ac54d1c2433ec6a68fd791274d3fc2e24614842
Nickhil Sethi <ns1635@nyu.edu>
2020-08-06 15:27:43 -0400

    custom jsonify working

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"compress/gzip"
 	"container/heap"
 	"encoding/json"
@@ -19,6 +20,41 @@ import (
 // an unfortunate hack to tolerate
 // unstructured JSON
 type jsonRow map[string]interface{}
+
+func (j jsonRow) MarshalJSON() ([]byte, error) {
+	buffer := bytes.NewBufferString("{")
+	length := len(j)
+	count := 0
+	for key, value := range j {
+		switch value.(type) {
+		case int:
+			buffer.WriteString(fmt.Sprintf("\"%s\":%d", key, value))
+			count++
+			if count < length {
+				buffer.WriteString(",")
+			}
+		case string:
+			buffer.WriteString(fmt.Sprintf("\"%s\":%s", key, value))
+			count++
+			if count < length {
+				buffer.WriteString(",")
+			}
+		default:
+			jsonified, err := json.Marshal(value)
+			if err != nil {
+				return nil, err
+			}
+			add := fmt.Sprintf("\"%s\":%s", key, jsonified)
+			buffer.WriteString(add)
+			count = count + len(add)
+			if count < length {
+				buffer.WriteString(",")
+			}
+		}
+	}
+	buffer.WriteString("}")
+	return buffer.Bytes(), nil
+}
 
 type resultRow struct {
 	stringContent string
