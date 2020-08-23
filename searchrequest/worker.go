1a4ac1a053e1698af93468d32be852fad4cd2bd8
Nickhil Sethi <ns1635@nyu.edu>
2020-08-23 17:11:08 -0400

    using match instead of find

--- searchrequest/worker.go
+++ searchrequest/worker.go
@@ -131,7 +131,7 @@ func (w *fileWorker) filterRow(row ResultRow) {
 	}
 
 	var rowBytes []byte
-	var match []byte
+	var match bool
 
 	if w.ParseJSON {
 		rowBytes, _ = json.Marshal(row.jsonContent)
@@ -139,8 +139,8 @@ func (w *fileWorker) filterRow(row ResultRow) {
 		rowBytes = []byte(row.stringContent)
 	}
 
-	match = w.Pattern.Find(rowBytes)
-	if match == nil {
+	match = w.Pattern.Match(rowBytes)
+	if !match {
 		return
 	}
 
